7ed01219a86d37250939915a020b6240da7e8a04
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2025-08-16 23:26:35 +0000

    d

--- app.py
+++ app.py
@@ -227,4 +227,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True, port=8000, host='0.0.0.0', allow_unsafe_werkzeug=True)
